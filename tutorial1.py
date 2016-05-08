d7f9bb05f20ee05efe4e8814a723c329eae22dda
eddennison <eddennison@users.noreply.github.com>
2016-05-08 12:10:44 -0400

    Updated from Brython Server: 5/8/2016 12:10:43 PM

--- tutorial1.py
+++ tutorial1.py
@@ -33,7 +33,7 @@ rpolygon = PolygonAsset([(0,250),
 Sprite(brectangle,(100,100))
 Sprite(rrectangle,(110,110))
 Sprite(bellipse,(500,250))
-Sprite(rpolygon(500,250))
+Sprite(rpolygon, (500,250))
 
 myapp = App()
 myapp.run()
