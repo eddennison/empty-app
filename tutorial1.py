70e1659961edd099325a0e2da8684f781c2f7635
eddennison <eddennison@users.noreply.github.com>
2016-05-08 12:26:40 -0400

    Updated from Brython Server: 5/8/2016 12:26:39 PM

--- tutorial1.py
+++ tutorial1.py
@@ -7,6 +7,7 @@ from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # Three primary colors with no transparency (alpha = 1.0)
 red = Color(0xff0000, 1.0)
+redtransparent = Color(0xff0000, 0.5)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
@@ -18,7 +19,7 @@ brectangle = RectangleAsset(50, 20, thinline, blue)
 rrectangle = RectangleAsset(50, 20, thinline, red)
 bellipse = EllipseAsset(500,250,thinline, blue)
 rpolygon = PolygonAsset([(50,0),(950,0),(1000,50),(1000,450),
-       (950,500),(50,500),(0,450),(0,50),(50,0)],thinline, red)
+       (950,500),(50,500),(0,450),(0,50),(50,0)],thinline, (red)
 
 
 
