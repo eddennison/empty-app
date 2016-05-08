44cffd6a5e4c7ca2da7d8e3d9bcb44ac966ce4c9
eddennison <eddennison@users.noreply.github.com>
2016-05-08 11:55:13 -0400

    Updated from Brython Server: 5/8/2016 11:55:12 AM

--- tutorial1.py
+++ tutorial1.py
@@ -19,7 +19,7 @@ rrectangle = RectangleAsset(50, 20, thinline, red)
 
 # Now display a rectangle
 Sprite(brectangle,(100,100))
-Sprite(rrectangle,(120,120))
+Sprite(rrectangle,(110,110))
 
 myapp = App()
 myapp.run()
