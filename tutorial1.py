0c582c8e5ec8caf5ecb23b0cda5702eb1f570892
eddennison <eddennison@users.noreply.github.com>
2016-05-08 12:14:12 -0400

    Updated from Brython Server: 5/8/2016 12:14:11 PM

--- tutorial1.py
+++ tutorial1.py
@@ -17,16 +17,16 @@ thinline = LineStyle(1, black)
 brectangle = RectangleAsset(50, 20, thinline, blue)
 rrectangle = RectangleAsset(50, 20, thinline, red)
 bellipse = EllipseAsset(500,250,thinline, blue)
-rpolygon = PolygonAsset([(0,250),
-                        (0,50),
-                        (50,0),
-                        (950,0),
+rpolygon = PolygonAsset([(0,0),
+                        (10,0),
+                        (10,10),
+                        (0,10),
                         (1000,50),
                         (1000,250),
                         (1000,450),
                         (950,50),
                         (50,500),
-                        (0,250)],thinline, red)
+                        (0,0)],thinline, red)
 
 
 # Now display a rectangle
